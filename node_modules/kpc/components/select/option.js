b9d94598330ac727d07cc735303bd657dd268728
SHENLISHA <shenlisha@kingsoft.com>
2018-09-13 22:26:01 +0800

    ls

--- node_modules/kpc/components/select/option.js
+++
@@ -1,6 +0,0 @@
-import Intact from 'intact';
-import {DropdownItem} from '../dropdown';
-
-export default class Option extends DropdownItem {
-
-}
--- node_modules/kpc/components/table/row.js
+++
@@ -1,7 +0,0 @@
-import Intact from 'intact';
-import template from './row.vdt';
-
-// for tr $destroyed event
-export default class TableRow extends Intact {
-    get template() { return template; }
-}
